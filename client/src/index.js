887ce618ac007279966eef639b98165461492553
Jinseok Oh <ohjinseok1113@gmail.com>
2022-11-18 20:23:04 +0900

    gitignore 캐시 삭제 & 채널톡 설치

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ChannelService from "./components/ChannelService";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// Boot Channel as an anonymous user
+ChannelService.boot({
+  "pluginKey": `${process.env.REACT_APP_CHANNEL_TALK_KEY}` //please fill with your plugin key
+});
+console.log(process.env.REACT_APP_CHANNEL_TALK_KEY);
 root.render(
   <React.StrictMode>
     <App />
