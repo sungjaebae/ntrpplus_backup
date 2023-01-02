00879fcdb15e0fe03b0c54df4ad1c8b50b032fb6
sungjae bae <hsm0156@gmail.com>
2023-01-02 04:48:46 +0000

    staging 서버 추가

--- server/certificate-generator.staging.sh
+++ server/certificate-generator.staging.sh
@@ -4,32 +4,31 @@ SSL_Path=/etc/letsencrypt/live/$domain
 
 mkdir -p ./certbot/conf/live/$domain
 
-docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.staging.yml run --rm --entrypoint "\
     openssl req -x509 -nodes \
     -days 365 \
     -newkey rsa:2048 \
     -keyout '$SSL_Path/privkey.pem' \
     -out '$SSL_Path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
-docker-compose -f docker-compose.dev.yml up --force-recreate -d nginx
+docker-compose -f docker-compose.staging.yml up --force-recreate -d nginx
 
-docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.staging.yml run --rm --entrypoint "\
   rm -rf /etc/letsencrypt/live/$domain && \
   rm -rf /etc/letsencrypt/archive/$domain && \
   rm -rf /etc/letsencrypt/renewal/$domain.conf" certbot
 
-docker-compose -f docker-compose.dev.yml  run --rm --entrypoint "\
+docker-compose -f docker-compose.staging.yml  run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
     --email hsm0156@gmail.com \
     -d $domain \
-    -d www.$domain \
     --rsa-key-size 4096 \
     --agree-tos \
     --no-eff-email \
     --force-renewal" certbot
 
-docker-compose -f docker-compose.dev.yml exec nginx nginx -s reload
+docker-compose -f docker-compose.staging.yml exec nginx nginx -s reload
 
 # renew
-# docker-compose -f docker-compose.dev.yml  run --rm --entrypoint "`
+# docker-compose -f docker-compose.staging.yml  run --rm --entrypoint "`
 #     certbot renew" certbot
